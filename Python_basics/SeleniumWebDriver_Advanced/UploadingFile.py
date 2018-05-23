881dc0a493ca44ea9a5ca4ac14a97889676b2068
venkateshwara.d <venkateshwara.d@RISMAA01DTP027.redim.net>
2018-05-23 15:57:15 +0530

    uploading a file working copy

--- Python_basics/SeleniumWebDriver_Advanced/UploadingFile.py
+++ Python_basics/SeleniumWebDriver_Advanced/UploadingFile.py
@@ -7,8 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import os
-from selenium.webdriver.common.keys import Keys
-#import pyperclip
 
 
 chrome_driver_path = os.path.abspath('..')  + "\\Drivers\\chromedriver.exe"
@@ -20,17 +18,14 @@ driver.implicitly_wait(5)
         
         # enter the file path onto the file-selection input field
 uploadElement  = driver.find_element(By.ID, "uploadfile_0")
-uploadElement.click()
-time.sleep(10)
-filename = os.path.dirname(__file__)  + "\\128 Interview-Questions.pdf"
-uploadElement.send_keys(filename)
-        
 
-        #pyperclip.copy('This is copied to the clipboard.')
- 
-        #pyperclip.paste()
-        # check the "I accept the terms of service" check box
+time.sleep(10)
+
+uploadElement.send_keys("D:\\webtable.JPG")
+
 time.sleep(2)
 driver.find_element(By.ID, "terms").click()
         # click the "UploadFile" button
 driver.find_element(By.NAME, "send").click()
+
+driver.quit()
