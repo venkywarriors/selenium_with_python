6189b6193ed498c6610c558252dc8a87adf46748
venkateshwara Doijode <33220328+venkywarriors619@users.noreply.github.com>
2018-05-25 15:26:26 +0530

    Update AllLinks.py

--- Python_basics/LaunchBrowsers/AllLinks.py
+++ Python_basics/LaunchBrowsers/AllLinks.py
@@ -10,10 +10,8 @@ chrome_driver_path = os.path.dirname(__file__)  + "\chromedriver.exe"
  
 driver=webdriver.Chrome(chrome_driver_path)
 
-
 driver.get('https://www.w3.org/')
 for a in driver.find_elements_by_xpath('.//a'):
     print(a.get_attribute('href'))
-    
     
 driver.close()    
