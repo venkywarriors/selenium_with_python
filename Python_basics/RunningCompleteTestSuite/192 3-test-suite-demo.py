24f70ac5161df5542428c390546fef81d754bed6
venkateshwara Doijode <33220328+venkywarriors619@users.noreply.github.com>
2018-05-25 14:25:31 +0530

    Update 192 3-test-suite-demo.py

--- Python_basics/RunningCompleteTestSuite/192 3-test-suite-demo.py
+++ Python_basics/RunningCompleteTestSuite/192 3-test-suite-demo.py
@@ -1,3 +1,7 @@
+'''
+Created on May 30, 2018
+@author: venkateshwara.d
+'''
 import unittest
 from tests.home.login_tests import LoginTests
 from tests.courses.register_courses_csv_data import RegisterCoursesCSVDataTests
