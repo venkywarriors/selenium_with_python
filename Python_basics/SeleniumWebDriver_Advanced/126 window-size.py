e96c216ebd16a09751f3f1686c1f1f75f95047f9
venkateshwara Doijode <33220328+venkywarriors619@users.noreply.github.com>
2018-05-25 13:04:46 +0530

    Update 126 window-size.py

--- Python_basics/SeleniumWebDriver_Advanced/126 window-size.py
+++ Python_basics/SeleniumWebDriver_Advanced/126 window-size.py
@@ -1,3 +1,7 @@
+'''
+Created on May 30, 2018
+@author: venkateshwara.d
+'''
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -13,13 +17,11 @@ class WindowSize():
         driver.get("https://letskodeit.teachable.com/pages/practice")
         driver.implicitly_wait(3)
 
-
         height = driver.execute_script("return window.innerHeight;")
         width = driver.execute_script("return window.innerWidth;")
         print("Height: " + str(height))
         print("Width: " + str(width))
         driver.quit()
-
 
 ff = WindowSize()
 ff.test()
