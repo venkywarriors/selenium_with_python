ccfae0337334461a7afedfa32c9af7012d47002a
venkateshwara Doijode <33220328+venkywarriors619@users.noreply.github.com>
2018-05-25 15:19:12 +0530

    Update interactingWithPage.py

--- Python_basics/LaunchBrowsers/interactingWithPage.py
+++ Python_basics/LaunchBrowsers/interactingWithPage.py
@@ -9,19 +9,15 @@ chrome_driver_path = os.path.dirname(__file__)  + "\chromedriver.exe"
  
 driver=webdriver.Chrome(chrome_driver_path)
 
-
 driver.get('http://codepad.org')
  
 text_area = driver.find_element_by_id('textarea')
 text_area.send_keys("This text is send using Python code.")
 
-
 text = driver.find_element_by_xpath("//*[@id='editor-form']/table/tbody/tr[1]/td/span").text
 print(text)
 
 print (driver.current_url)
-
-
 
 driver.close()
 #driver.forward()
