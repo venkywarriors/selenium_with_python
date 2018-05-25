1b7598ecf738ca5310167d26bbdd8dcc349aec9c
venkateshwara Doijode <33220328+venkywarriors619@users.noreply.github.com>
2018-05-25 13:13:28 +0530

    Update UploadingFile.py

--- Python_basics/SeleniumWebDriver_Advanced/UploadingFile.py
+++ Python_basics/SeleniumWebDriver_Advanced/UploadingFile.py
@@ -8,24 +8,19 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-
 chrome_driver_path = os.path.abspath('..')  + "\\Drivers\\chromedriver.exe"
  
 driver=webdriver.Chrome(chrome_driver_path)
 driver.maximize_window()
 driver.get("http://demo.guru99.com/test/upload/")
 driver.implicitly_wait(5)
-        
-        # enter the file path onto the file-selection input field
+# enter the file path onto the file-selection input field
 uploadElement  = driver.find_element(By.ID, "uploadfile_0")
-
 time.sleep(10)
-
 uploadElement.send_keys("D:\\webtable.JPG")
-
 time.sleep(2)
 driver.find_element(By.ID, "terms").click()
-        # click the "UploadFile" button
+# click the "UploadFile" button
 driver.find_element(By.NAME, "send").click()
 
 driver.quit()
